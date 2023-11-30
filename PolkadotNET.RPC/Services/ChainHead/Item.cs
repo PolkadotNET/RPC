def3f93cf2a619286322884a97baaaa1ce6e22e4
Daniel Moos <daniel.moos@outlook.com>
2023-11-30 11:22:07 +0100

    bump

--- PolkadotNET.RPC/Services/ChainHead/Item.cs
+++ PolkadotNET.RPC/Services/ChainHead/Item.cs
@@ -3,6 +3,6 @@
 namespace PolkadotNET.RPC.Services.ChainHead;
 
 
-// todo: Type -> Enum
+// todo: Type -> Enum and serialize
 [DataContract]
 public record Item([property: DataMember(Name = "key")] string Key, [property: DataMember(Name = "type")] string Type);
